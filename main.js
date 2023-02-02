0cc3f284e8f3b4f44d398b13d6654a6c282d91db
Shreshtha Sodhia <shreshtha.sodhia@somaiya.edu>
2023-02-03 00:21:24 +0530

    2:00:00

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const navItems=document.querySelector('.nav__items');
 const openNavBtn=document.querySelector('#open__nav-btn');
 const closeNavBtn=document.querySelector('#close__nav-btn');
 
-const openNav=()=>
+
 
 openNavBtn.addEventListener('click',openNav);
 
